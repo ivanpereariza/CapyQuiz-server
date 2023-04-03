6d0c17fc7a4c28935ee5c475bab9e99e999ad44c
Ivan Pereiro <ivanpereariza@gmail.com>
2023-04-03 14:49:23 +0200

    socket

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const app = require("./app")
-const http = require('http').createServer(app)
-const io = require('socket.io')(http, {
-  cors: {
-    origin: process.env.ORIGIN,
-    methods: ["GET", "POST"]
-  }
-})
+const server = require('http').createServer(app)
+require("./config/socket.config")(server)
 
 const PORT = process.env.PORT || 5005;
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server (●'◡'●) listening on http://localhost:${PORT}`);
-})
-
-io.on('connection', (socket) => {
-  console.log('New user connected')
-
-  socket.on('clientConnected', () => {
-    console.log('Client connected:', socket.id)
-  })
-
-  socket.on("sendNotification", (data) => {
-    io.emit("getNotification", data)
-  })
 })
